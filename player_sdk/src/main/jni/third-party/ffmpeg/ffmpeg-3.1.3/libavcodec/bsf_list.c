81cf616e1654b58eee1e6c2b879a933d84db0566
chris.zhang <zhangyanlongcodec@gmail.com>
2016-11-15 11:05:01 +0800

    Remove FFMPEG-3.1.3 source files
    Remove FFMPEG-3.1.3 source files

--- player_sdk/src/main/jni/third-party/ffmpeg/ffmpeg-3.1.3/libavcodec/bsf_list.c
+++
@@ -1,4 +0,0 @@
-static const AVBitStreamFilter *bitstream_filters[] = {
-    &ff_aac_adtstoasc_bsf,
-    &ff_h264_mp4toannexb_bsf,
-    NULL };
--- player_sdk/src/main/jni/third-party/ffmpeg/ffmpeg-3.1.3/libavformat/protocol_list.c
+++
@@ -1,6 +0,0 @@
-static const URLProtocol *url_protocols[] = {
-    &ff_file_protocol,
-    &ff_hls_protocol,
-    &ff_http_protocol,
-    &ff_tcp_protocol,
-    NULL };
